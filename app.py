2a88afb53b0b9f97e885337e32183d09e9e99d3a
Deepak Moud <deepakmoud@poornima.org>
2023-02-14 15:56:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 st.set_option('deprecation.showfileUploaderEncoding', False)
 # Load the pickled model
-model = pickle.load(open('/content/drive/My Drive/restuarantreviewnlp.pkl','rb'))   
+model = pickle.load(open('restuarantreviewnlp.pkl','rb'))   
 
 
 def review(text):
-  dataset = pd.read_csv('/content/drive/My Drive/Restaurant_Reviews.tsv', delimiter="\t", quoting=3)
+  dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter="\t", quoting=3)
   # First step: cleaning Text and removing number and punctuation marks.
   # Cleaning the texts for all review using for loop
   import re
